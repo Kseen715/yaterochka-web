7958e70485502effd8cdc618997f5998ceff597b
kseen <korenevdn@gmail.com>
2023-12-08 21:52:02 +0300

    New CI/CD script!

--- docker-push.ps1
+++ docker-push.ps1
@@ -1,5 +1,5 @@
 # Usually this is the version
-$tag = "1.0"
+$tag = "1.0.1"
 
 # Name of the image
 # in format "<dockerhub-account-name>/<imange-name>"
@@ -9,4 +9,7 @@ $tag = "1.0"
 $name = "kseen/yaterochka-web"
 
 # Build and push as tagged & latest images
-(docker build -t ${name} -t ${name}:${tag} . || Write-Error 'Build error.' -ForegroundColor Red) && (docker push ${name}:${tag} || Write-Error 'Tag push error.' -ForegroundColor Red) && (docker push ${name} || Write-Error 'Latest tag push error.' -ForegroundColor Red) && (Write-Host 'Success!' -ForegroundColor Green)
+(docker build -t ${name} -t ${name}:${tag} . || Write-Error 'Build error.') && (docker push ${name}:${tag} || Write-Error 'Tag push error.') && (docker push ${name} || Write-Error 'Latest tag push error.') && (docker rmi ${name}:${tag} || Write-Error 'Deleting local tagged image error.') && (Write-Host 'Success!' -ForegroundColor Green)
+
+# Delete dangling images
+(docker image prune -f || Write-Host 'Dangling images deleted.')
