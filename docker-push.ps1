d56460198ac252958506158098b3ac9b7f9cc368
kseen <korenevdn@gmail.com>
2023-12-08 21:32:21 +0300

    Add comments

--- docker-push.ps1
+++ docker-push.ps1
@@ -1,4 +1,12 @@
+# Usually this is the version
 $tag = "1.0"
+
+# Name of the image
+# in format "<dockerhub-account-name>/<imange-name>"
+# or "ghcr.io/<github-account-name>/<imange-name>" 
+# (if you want to push to GitHub Container Registry
+# instead of Docker Hub)
 $name = "kseen/yaterochka-web"
 
+# Build and push as tagged & latest images
 (docker build -t ${name} -t ${name}:${tag} . || Write-Error 'Build error.' -ForegroundColor Red) && (docker push ${name}:${tag} || Write-Error 'Tag push error.' -ForegroundColor Red) && (docker push ${name} || Write-Error 'Latest tag push error.' -ForegroundColor Red) && (Write-Host 'Success!' -ForegroundColor Green)
